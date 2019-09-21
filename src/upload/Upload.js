3d9ac01cd43a1bccab2c7c9d7f89cf18b1f3add1
softdev1029 <softdev1029@outlook.com>
2019-09-21 16:03:50 +0800

    fix ip

--- src/upload/Upload.js
+++ src/upload/Upload.js
@@ -73,7 +73,7 @@ class Upload extends Component {
       const formData = new FormData();
       formData.append("file", file, file.name);
 
-      req.open("POST", "http://localhost:8000/upload");
+      req.open("POST", "http://104.192.5.41:8000/upload");
       req.send(formData);
     });
   }
